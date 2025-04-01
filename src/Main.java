e414d2422eef8822ab87fa77b208c530dccae19b
Harumb1 <97941428+Harumb1@users.noreply.github.com>
2025-04-01 16:47:33 +0300

    Update Main.java
    Tasks are now saved in the ListOfTasks text file

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     static String choice;
     static String newTask;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("  _______    _____              _     _      _ \n" +
                 " |__   __|  |  __ \\            | |   | |    | |\n" +
@@ -23,36 +23,43 @@ public class Main {
         setChoice();
     }
 
-    static void setChoice() {
+    static void setChoice() throws IOException {
         Scanner sc = new Scanner(System.in);
         switch (choice) {
             case "1":
                 System.out.print("How would you like to name your new task?\n");
                 newTask = sc.nextLine();
-
                 try {
-                    File myObj = new File(newTask + ".txt");
+                    File task = new File(newTask + ".txt");
                     //https://stackoverflow.com/questions/1625234/how-to-append-text-to-an-existing-file-in-java
                     //https://stackoverflow.com/questions/34836789/how-to-append-java-output-to-text-file
-                    if (myObj.createNewFile()) {
+                    if (task.createNewFile()) {
                         String input = null;
-                        System.out.println("New Task Created:\n" + newTask);
-                        System.out.println("Your task can now receive input: ");
+                        System.out.println("New Task Created!\n" + "\"" + newTask + "\"");
+                        System.out.println("=================================================" + "\n");
                         while (!"exit".equalsIgnoreCase(input = sc.nextLine())) {
-                            FileWriter writer = new FileWriter(myObj, true);
+                            FileWriter writer = new FileWriter(task, true);
+//                            taskList.append(newTask);
                             writer.append(input).append("\n");
 
                             writer.flush();
                             writer.close();
                         }
-
                     } else {
                         System.out.println("File already exists.");
                     }
-
                 } catch (IOException e) {
                     System.out.println("An error occurred.");
                     e.printStackTrace();
+                }
+
+                FileWriter taskList = new FileWriter("C:\\Users\\alexs\\IdeaProjects\\TaskManagerList\\ListOfTasks.txt", true);
+                try {
+                    taskList.append(newTask + "\n");
+                    taskList.flush();
+                    taskList.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
                 break;
             case "2":
