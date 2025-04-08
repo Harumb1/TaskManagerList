4f369e5148c5bb835f85a4c8855bdfa273ddd3d0
Harumb1 <97941428+Harumb1@users.noreply.github.com>
2025-04-08 19:55:46 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,46 +12,7 @@ public class Main {
         setChoice();
     }
 
-    static void mainMenu() throws IOException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("  _______    _____              _     _      _ \n" +
-                " |__   __|  |  __ \\            | |   | |    | |\n" +
-                "    | | ___ | |  | | ___   __ _| |__ | | ___| |\n" +
-                "    | |/ _ \\| |  | |/ _ \\ / _` | '_ \\| |/ _ \\ |\n" +
-                "    | | (_) | |__| | (_) | (_| | |_) | |  __/_|\n" +
-                "    |_|\\___/|_____/ \\___/ \\__,_|_.__/|_|\\___(_)");
-
-        System.out.println("\n                  1.NEW TASK");
-        System.out.println("                  2.OPEN AN EXISTING TASK");
-        System.out.println("                  3.EXIT");
-        choice = sc.nextLine();
-        setChoice();
-    }
-
-    static void listOfTasks(){
-        System.out.println("  _      _     _    ____   __ _______        _        _ \n" +
-                " | |    (_)   | |  / __ \\ / _|__   __|      | |      | |\n" +
-                " | |     _ ___| |_| |  | | |_   | | __ _ ___| | _____| |\n" +
-                " | |    | / __| __| |  | |  _|  | |/ _` / __| |/ / __| |\n" +
-                " | |____| \\__ \\ |_| |__| | |    | | (_| \\__ \\   <\\__ \\_|\n" +
-                " |______|_|___/\\__|\\____/|_|    |_|\\__,_|___/_|\\_\\___(_)");
-        System.out.println("LIST OF TASKS:");
-        try (BufferedReader reader = new BufferedReader(new FileReader("ListOfTasks.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Could not locate file.");
-        } catch (IOException e) {
-            System.out.println("Something went wrong.");
-        }
-    }
-
-    static void readTask(){
-
-    }
-
+    //https://stackoverflow.com/questions/822150/modify-a-txt-file-in-java
     static void setChoice() throws IOException {
         Scanner sc = new Scanner(System.in);
         switch (choice) {
@@ -66,9 +27,10 @@ public class Main {
                 System.out.print("Your New Task's Name:\n");
                 newTask = sc.nextLine();
                 try {
+                    String input;
                     File task = new File(newTask + ".txt");
                     if (task.createNewFile()) {
-                        String input = null;
+                        input = null;
                         System.out.println("New Task Created!\n" + "\"" + newTask + "\"");
                         System.out.println("============================================================================");
                         while (!"exit".equalsIgnoreCase(input = sc.nextLine())) {
@@ -77,8 +39,7 @@ public class Main {
                             writer.flush();
                             writer.close();
                         }
-                    }
-                    else {
+                    } else {
                         System.out.println("File already exists.");
                     }
                 } catch (IOException e) {
@@ -101,25 +62,30 @@ public class Main {
                 listOfTasks();
 
                 while (true) {
+
                     System.out.println("\nType:");
                     System.out.println(" |OPEN| to read an existing task");
                     System.out.println(" |EDIT| to modify an existing task");
                     System.out.println(" |EXIT| to return to the main menu");
                     exitChoice = sc.nextLine();
+
                     //exit
                     if (exitChoice.equalsIgnoreCase("exit")) {
                         mainMenu();
                         setChoice();
                         break;
                     }
-                     if (exitChoice.equalsIgnoreCase("edit")) {
+                    //edit
+                    if (exitChoice.equalsIgnoreCase("edit")) {
                         System.out.println("Which file would you like to edit?");
-
+                        exitChoice = sc.nextLine();
                         break;
                     }
-                     if (exitChoice.equalsIgnoreCase("open")) {
+                    //open
+                    if (exitChoice.equalsIgnoreCase("open")) {
                         System.out.println("Which file would you like to OPEN?");
-
+                        readTask();
+                        exitChoice = sc.nextLine();
                         break;
                     }
                     else {
@@ -129,16 +95,68 @@ public class Main {
                 }
                 break;
 
-            //Case "Exit"
-            case "3":
-                System.out.print("See you soon :)");
-                break;
-            default:
-                System.out.println("There is no such command!");
-                choice = sc.nextLine();
-                setChoice();
-                //recursion
-                break;
+                    //Case "Exit"
+                    case "3":
+                        System.out.print("See you soon :)");
+                        break;
+                    default:
+                        System.out.println("There is no such command!");
+                        choice = sc.nextLine();
+                        setChoice();
+                        //recursion
+                        break;
         }
+    }
+
+    static void mainMenu() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("  _______    _____              _     _      _ \n" +
+                " |__   __|  |  __ \\            | |   | |    | |\n" +
+                "    | | ___ | |  | | ___   __ _| |__ | | ___| |\n" +
+                "    | |/ _ \\| |  | |/ _ \\ / _` | '_ \\| |/ _ \\ |\n" +
+                "    | | (_) | |__| | (_) | (_| | |_) | |  __/_|\n" +
+                "    |_|\\___/|_____/ \\___/ \\__,_|_.__/|_|\\___(_)");
+
+        System.out.println("\n                  1.NEW TASK");
+        System.out.println("                  2.OPEN AN EXISTING TASK");
+        System.out.println("                  3.EXIT");
+        choice = sc.nextLine();
+        setChoice();
+    }
+
+    static void listOfTasks() {
+        System.out.println("  _      _     _    ____   __ _______        _        _ \n" +
+                " | |    (_)   | |  / __ \\ / _|__   __|      | |      | |\n" +
+                " | |     _ ___| |_| |  | | |_   | | __ _ ___| | _____| |\n" +
+                " | |    | / __| __| |  | |  _|  | |/ _` / __| |/ / __| |\n" +
+                " | |____| \\__ \\ |_| |__| | |    | | (_| \\__ \\   <\\__ \\_|\n" +
+                " |______|_|___/\\__|\\____/|_|    |_|\\__,_|___/_|\\_\\___(_)");
+        System.out.println("LIST OF TASKS:");
+        try (BufferedReader reader = new BufferedReader(new FileReader("ListOfTasks.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not locate file.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong.");
+        }
+    }
+
+    static void readTask() {
+        Scanner sc = new Scanner(System.in);
+        editChoice = sc.nextLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader(editChoice + ".txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not locate file.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong.");
+        }
+
     }
 }
